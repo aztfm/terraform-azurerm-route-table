e98f29c8e577c5f7abd820a4835d613f382cb5c2
Daniel Garrido <gareda95@outlook.es>
2021-11-28 09:24:50 +0100

    Internal changes

--- examples/route-table-with-routes/main.tf
+++ examples/route-table-with-routes/main.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "TFAZRT" {
-  name     = "terraform-azurerm-route-table"
+resource "azurerm_resource_group" "rg" {
+  name     = "resource-group"
   location = "West Europe"
 }
 
-module "TFAZRT-rt" {
+module "route_table" {
   source              = "./terraform-azurerm-route-table"
-  name                = "TFAZRT-rt"
-  resource_group_name = azurerm_resource_group.TFAZRT.name
-  location            = azurerm_resource_group.TFAZRT.location
+  name                = "route-table"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   routes = [
     { name = "route1", address_prefix = "10.0.0.0/16", next_hop_type = "VirtualAppliance", next_hop_in_ip_address = "10.0.0.4" },
     { name = "route2", address_prefix = "10.10.0.0/16", next_hop_type = "VirtualAppliance", next_hop_in_ip_address = "10.0.0.4" },
--- variables.tf
+++ variables.tf
@@ -1,29 +1,33 @@
-variable name {
+variable "name" {
   type        = string
   description = "The name of the route table."
 }
-variable resource_group_name {
+
+variable "resource_group_name" {
   type        = string
   description = "The name of the resource group in which to create the route table."
 }
-variable location {
+
+variable "location" {
   type        = string
   description = "The location/region where the route table is created."
 }
-variable routes {
+
+variable "routes" {
   type        = list(map(string))
   default     = []
   description = "List of objects that represent the configuration of each route."
   /*ROUTES = [{ name = "", address_prefix = "", next_hop_type = "", next_hop_in_ip_address = "" }]*/
 }
-variable disable_bgp_route_propagation {
+
+variable "disable_bgp_route_propagation" {
   type        = bool
   default     = true
   description = "Boolean flag which controls propagation of routes learned by BGP on that route table."
 }
-variable tags {
+
+variable "tags" {
   type        = map(string)
   default     = {}
   description = "A mapping of tags to assign to the resource."
 }
-
