b3fb3fca117564f634ef31edc1663581abcb0a98
Daniel Garrido Sánchez <gareda95@outlook.es>
2023-06-21 18:14:35 +0200

    Update module

--- main.tf
+++ main.tf
@@ -1,16 +1,18 @@
 resource "azurerm_route_table" "rt" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  name                          = var.name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  tags                          = var.tags
+  disable_bgp_route_propagation = var.disable_bgp_route_propagation
+
   dynamic "route" {
     for_each = var.routes
+
     content {
       name                   = route.value.name
       address_prefix         = route.value.address_prefix
       next_hop_type          = route.value.next_hop_type
-      next_hop_in_ip_address = lookup(route.value, "next_hop_in_ip_address", null)
+      next_hop_in_ip_address = route.value.next_hop_in_ip_address
     }
   }
-  disable_bgp_route_propagation = var.disable_bgp_route_propagation
-  tags                          = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -13,21 +13,25 @@ variable "location" {
   description = "The location/region where the route table is created."
 }
 
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "A mapping of tags to assign to the resource."
+}
+
 variable "routes" {
-  type        = list(map(string))
+  type = list(object({
+    name                   = string
+    address_prefix         = string
+    next_hop_type          = string
+    next_hop_in_ip_address = optional(string)
+  }))
   default     = []
   description = "List of objects that represent the configuration of each route."
-  /*ROUTES = [{ name = "", address_prefix = "", next_hop_type = "", next_hop_in_ip_address = "" }]*/
 }
 
 variable "disable_bgp_route_propagation" {
   type        = bool
   default     = true
   description = "Boolean flag which controls propagation of routes learned by BGP on that route table."
-}
-
-variable "tags" {
-  type        = map(string)
-  default     = {}
-  description = "A mapping of tags to assign to the resource."
 }
