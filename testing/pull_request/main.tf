9808fca330da19493c320607e95fb23d25c2d937
Daniel Garrido Sánchez <gareda95@outlook.es>
2023-06-21 18:18:39 +0200

    Update testing

--- testing/pull_request/main.tf
+++ testing/pull_request/main.tf
@@ -4,14 +4,14 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "route_table" {
-  source              = "./module"
-  name                = azurerm_resource_group.rg.name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  source                        = "./module"
+  name                          = azurerm_resource_group.rg.name
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
+  disable_bgp_route_propagation = false
   routes = [
     { name = "route1", address_prefix = "10.0.0.0/16", next_hop_type = "VirtualAppliance", next_hop_in_ip_address = "10.0.0.4" },
     { name = "route2", address_prefix = "10.10.0.0/16", next_hop_type = "VirtualAppliance", next_hop_in_ip_address = "10.0.0.4" },
     { name = "route3", address_prefix = "0.0.0.0/0", next_hop_type = "Internet" }
   ]
-  disable_bgp_route_propagation = false
 }
